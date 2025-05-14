136389ab3ebcc12ab8ebfe1cac2a7d0210d6cb5f
diztinct-glenn <glenn@diztinct.com>
2025-05-14 09:38:18 -0400

    page change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,27 +89,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="columns small-12">
-            <div className="c-item is-autoplay inview--visible" data-inview="">
-              <figure className="c-item__media">
-                <Link href="/work/project-4">
-                  <div className="c-item__image">
-                    <div className="intrinsic-image" style={{ paddingBottom: '60%' }}>
-                      <img src="https://placehold.co/600x400?text=Project+1" alt="Project 4" />
-                    </div>
-                  </div>
-                </Link>
-              </figure>
-              <div className="c-item__content">
-                <h2 className="c-item__title">
-                  <Link href="/work/project-4">Project 4</Link>
-                </h2>
-                <div className="c-item__text">Short description for Project 4.</div>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </>
   );
