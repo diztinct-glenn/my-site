b3420d599577cdcffb0ceb63d757914a32132747
diztinct-glenn <glenn@diztinct.com>
2025-06-03 23:36:47 -0400

    linter errors

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,7 @@ export default function AboutPage() {
       <div className="2xl:grid 2xl:grid-rows-6 2xl:grid-cols-12 2xl:gap-5  2xl:overflow-hidden 2xl:min-h-[90vh]">
         <div className="2xl:col-start-2 2xl:col-span-11 2xl:row-start-1 2xl:row-span-3">
           <p className="text-lg md:sticky md:top-24 z-10 mb-[20px]">
-            Hey, I'm Glenn — a software engineer who's all about making the web look good and work even better. I've had the chance to work with brands like the Florida Panthers and Lucid Motors, creating custom solutions that actually move the needle. Outside of code, you'll find me digging through record crates, saving way too many pins on Google Maps, or playing with my pup. Let's make something cool.
+            Hey, I&apos;m Glenn — a software engineer who&apos;s all about making the web look good and work even better. I&apos;ve had the chance to work with brands like the Florida Panthers and Lucid Motors, creating custom solutions that actually move the needle. Outside of code, you&apos;ll find me digging through record crates, saving way too many pins on Google Maps, or playing with my pup. Let&apos;s make something cool.
           </p>
         </div>
         <div className="2xl:flex 2xl:col-start-1 2xl:col-end-14 2xl:row-start-2 2xl:row-end-7">
--- src/components/MainWrapper.tsx
+++ src/components/MainWrapper.tsx
@@ -4,7 +4,6 @@ import React from "react";
 
 export default function MainWrapper({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-  const isAbout = pathname === "/about";
   return (
     <main className={`p-[20px] pb-[40px] md:py-[100px] md:grow`}>
       {children}
