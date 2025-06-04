2903909adaa1678678fa7342bd99ab9480ee177f
diztinct-glenn <glenn@diztinct.com>
2025-06-03 23:37:56 -0400

    sleepy

--- src/components/MainWrapper.tsx
+++ src/components/MainWrapper.tsx
@@ -3,7 +3,6 @@ import { usePathname } from "next/navigation";
 import React from "react";
 
 export default function MainWrapper({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
   return (
     <main className={`p-[20px] pb-[40px] md:py-[100px] md:grow`}>
       {children}
